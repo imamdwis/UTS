2c0dae6841c8cf03bfef6b896319ad81a954cad7
Imam <20102174@ittelkom-pwt.ac.id>
2023-05-18 09:23:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts/RegisterActivity.java
@@ -0,0 +1,47 @@
+package com.example.uts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUsername,edEmail,edPassword,edConfirm;
+    Button btn;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        edUsername = findViewById(R.id.editTextRegUsername);
+        edPassword = findViewById(R.id.editTextRegPassword);
+        edEmail = findViewById(R.id.editTextRegEmail);
+        edConfirm = findViewById(R.id.editTextRegConfirmPassword);
+        btn = findViewById(R.id.buttonRegister);
+        tv = findViewById(R.id.textViewExistingUser);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edUsername.getText().toString();
+                String email = edEmail.getText().toString();
+                String password = edPassword.getText().toString();
+                String confirm = edConfirm.getText().toString();
+            }
+        });
+    }
+}
